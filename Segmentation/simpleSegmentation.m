ea8aae5403d4111aa9498f719c9b4fa8f2e50433
warmflasha <warmflasha@gmail.com>
2022-12-12 17:54:09 -0600

    bfmatlab update

--- Segmentation/simpleSegmentation.m
+++ Segmentation/simpleSegmentation.m
@@ -1,4 +1,4 @@
-function mask = simpleSegmentation(img,cellSize,minIntensity)
+function mask = simpleSegmentation(img,cellSize,minIntensity,filterSize)
 % Very simple segmentation routine based on a local thresholding.
 % Normalizes image to the dilation and thresholds using 0.9 of max
 % Need to specify an object (cell) size 
@@ -14,8 +14,13 @@ if ~exist('minIntensity','var')
 end
 mask = img_local_max > 0.9 & img > minIntensity;
 
-sizefilt = floor(pi*[(cellSize/2)^2, (10*cellSize)^2]);
+if ~exist('filterSize','var')
+    filterSize = true;
+end
 
+if filterSize
+sizefilt = floor(pi*[(cellSize/2)^2, (10*cellSize)^2]);
 mask = bwareafilt(mask,sizefilt);
+end
 
 
