f7ba954e7732abf394f7222e6b4e2ff7d44552de
warmflasha <warmflasha@gmail.com>
2023-06-20 17:06:13 -0500

    comments for ImageDirectory

--- dataClasses/ImageDirectory.m
+++ dataClasses/ImageDirectory.m
@@ -1,23 +1,38 @@
 classdef ImageDirectory < handle
+    %Data class for storing information about a directory of images.  
     properties
-        path
-        prefix
-        plate
-        well
-        position
-        ordering
-        extension
-        pathstring
+        path %path to directory
+        prefix %prefix of image file names. Use only if separated by '_' from portions with numbers
+                    % e.g cells_plate1_well1_1.tif has prefix cells
+                    % 'p1_w1_1.tif' has no prefix
+        plate %array of plate numbers 1 entry per image
+        well   %array of well numbers 1 entry per image
+        position %array of position numbers 1 entry per image
+        ordering %cell array of strings that go with numbers. e.g. for p1_w1_1.tif etc ordering is {'p','w',''}. 
+        extension % file extension e.g. '.tif'
+        pathstring % string with %d for numbers to be used with sprintf for generating file names
     end
     methods
+
         function this = ImageDirectory(varargin)
+
+            %Simple constructor. If no inputs, just makes an empty object.
+            %Can supply the path to directory as one input. 
+
             if nargin == 0
                 return;
             elseif nargin == 1
                 this.path = varargin{1};
             end
         end
+        
         function setPositionNumbersFromRanges(this,plates,wells,positions)
+            
+            %Allow you to set plate, well, and position properties from
+            %arrays. Needs to be "rectangular" with equal numbers of
+            %positions for each well etc. cannot currently have missing
+            %files or these need to be removed manually. 
+
             npos = length(positions);
             nplates = length(plates);
             nwells = length(wells);
@@ -41,6 +56,11 @@ classdef ImageDirectory < handle
         end
 
         function filename = getFileName(this,imgNum,includeDirectory)
+            
+            %Returns the filename of the image at imgNum. if
+            %includeDirectory is true will include full path (default is
+            %false).
+            % can use getLinIndex to get index from plate, well, position
 
             if ~exist("includeDirectory","var")
                 includeDirectory = false;
@@ -63,6 +83,9 @@ classdef ImageDirectory < handle
         end
 
          function linIndex = getLinIndex(this,plate,well,pos)
+
+             %get the linear index given plate, well, position
+             
             linIndex =find( ismember(this.plate,plate) & ismember(this.well,well) & ismember(this.position,pos));
          end
     end
